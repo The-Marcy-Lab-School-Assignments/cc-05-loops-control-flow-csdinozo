78b8c4457a1ac8ca7acce97eb5695285e4a73e08
Catalina Dinozo <csdinozo@gmail.com>
2024-10-09 10:49:14 -0400

    p2 greater num

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const multiplesOfSixAndNine = () => {
 }
 // multiplesOfSixAndNine();
 
+// p2
+const greaterNum = (numOne, numTwo) => {
+	if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return null;
+	if (numOne === numTwo) return "both integers are equal";
+	else if (numOne > numTwo) return numOne;
+	return numTwo;
+}
+// console.log(greaterNum(1,"4"));
+// console.log(greaterNum(1,1));
+// console.log(greaterNum(1,2));
+// console.log(greaterNum(2,1));
+
